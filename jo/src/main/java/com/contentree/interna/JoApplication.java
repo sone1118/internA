1e7a6e3d9d8d2e180aac1170062681aa8d453637
DESKTOP-FRNFNLN\Joins <intern_A>
2022-11-24 14:01:21 +0900

    [74085] Setting : Swagger 설정

--- jo/src/main/java/com/contentree/interna/JoApplication.java
+++ jo/src/main/java/com/contentree/interna/JoApplication.java
@@ -2,18 +2,15 @@ package com.contentree.interna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Spring Boot 2.x
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JoApplication extends SpringBootServletInitializer {
  
     public static void main(String[] args) {
