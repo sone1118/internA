2d46de14d562747506d9ca81de1276cc5d22283c
손혜진 <sone5414@gmail.com>
2022-11-28 15:35:20 +0900

    Revert "[74052] Feat : 로그인 controller추가, FE 설정"
    This reverts commit 269405672fc816afc6abcd1dfdf9ddbc8ed89c91.

--- jo/src/main/java/com/contentree/interna/JoApplication.java
+++ jo/src/main/java/com/contentree/interna/JoApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
 // Spring Boot 2.x
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
 
 
 @SpringBootApplication
@@ -36,7 +34,7 @@ class HelloController {
     @RequestMapping("/hello/{name}")
     String hello(@PathVariable String name) {
     	
-        return "Hello " + name + " !";
+        return "Hi " + name + " !";
+ 
     }
 }
-
