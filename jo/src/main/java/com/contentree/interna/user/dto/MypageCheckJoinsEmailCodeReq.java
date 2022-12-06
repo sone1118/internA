b2cd35d6716d7c32f631dd63e16c9433d5e01f8a
김지슬 <kim.jiseul@joins.com>
2022-12-06 13:05:30 +0900

    [74532] Comment : Dto 추가 및 이름 추가

--- jo/src/main/java/com/contentree/interna/user/dto/MypageCheckJoinsEmailCodeReq.java
+++ jo/src/main/java/com/contentree/interna/user/dto/MypageCheckJoinsEmailCodeReq.java
@@ -3,6 +3,11 @@ package com.contentree.interna.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+/**
+ * 
+ * @author 김지슬
+ *
+ */
 @Getter
 public class MypageCheckJoinsEmailCodeReq {
 	@Schema(description = "이메일로 전달받은 8자리 코드", example = "EK3F1K0J")
